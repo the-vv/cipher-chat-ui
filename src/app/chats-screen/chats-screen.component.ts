1403debf60ff03a9e0fd9f33bb0628b73a9f846b
vv-2001 <vishnuvinod2772001@gmail.com>
2021-04-03 22:41:54 +0530

    before setting up auto scroll

--- src/app/chats-screen/chats-screen.component.ts
+++ src/app/chats-screen/chats-screen.component.ts
@@ -16,18 +16,18 @@ export class ChatsScreenComponent implements OnInit, OnChanges, AfterViewChecked
   hasSelected: boolean = false;
 
   @ViewChild('scrollDown')
-  myScrollContainer: ElementRef;
+  scrollContainer: ElementRef;
 
   @HostListener('document:keyup', ['$event'])
   handleInput(event: KeyboardEvent) {
-    if(event.code == 'Enter' && this.isChatSendable && this.messageString.length > 0) {
+    if (event.code == 'Enter' && this.isChatSendable && this.messageString.length > 0) {
       this.sendMessage();
-    }    
+    }
   }
 
-  @HostListener('scroll', ['$event'])
-  handleScroll($event) {
-    console.log($event, 'hllo');    
+
+  handleScroll(event) {
+    console.log(event, 'hllo');
   }
 
   onFocus() {
@@ -44,40 +44,39 @@ export class ChatsScreenComponent implements OnInit, OnChanges, AfterViewChecked
   messageString: string;
   isChatSendable: boolean = false;
   container: HTMLElement;
-  needChange: boolean = true;
+  needScroll: boolean = true;
 
   constructor(public socket: SocketService,
     private message: MessagesServiceService) {
   }
 
- 
+
   ngOnInit() {
     this.scrollToBottom();
   }
 
   ngAfterViewChecked() {
-    // if(this.needChange) {
+    if (this.needScroll) {
       this.scrollToBottom();
-      // this.needChange = false;
-    // }
+    }
   }
 
-  ngAfterViewInit() {    
+  ngAfterViewInit() {
   }
 
   scrollToBottom(): void {
     try {
-      this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+      this.scrollContainer.nativeElement.scrollTop = this.scrollContainer.nativeElement.scrollHeight;
     } catch (err) { }
   }
 
   sendMessage() {
-    if(this.messageString.length <=0 ) {
+    if (this.messageString.length <= 0) {
       return;
-    }  
-    this.needChange = true;
+    }
+    this.needScroll = true;
     this.message.sendMessage(this.chat._id, this.messageString);
-    this.messageString = '';  
+    this.messageString = '';
     // this.message.addNewChatTo(); 
   }
 
@@ -92,8 +91,8 @@ export class ChatsScreenComponent implements OnInit, OnChanges, AfterViewChecked
       this.randomColor = changes.chat.currentValue.color;
       this.messages = changes.chat.currentValue.messages;
       console.log(changes.chat.currentValue);
-      
-      this.needChange = true;
+
+      this.needScroll = true;
     }
   }
 
